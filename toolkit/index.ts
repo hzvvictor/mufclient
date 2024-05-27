ec005cfc8d76bbf342bb2e4f9dca995a56a6d71f
hzvvictor <hzvvictor@gmail.com>
2024-05-26 18:38:07 -0600

    feat: Add initSocketEvents function to toolkit

--- toolkit/index.ts
+++ toolkit/index.ts
@@ -4,10 +4,13 @@ import initSockeIo from "./initSocketIo";
 export { initSockeIo };
 import newHandler from "./newHandler";
 export { newHandler };
+import initSocketEvents from "./initSocketEvents";
+export { initSocketEvents };
 
 const toolkit = {
   fnHandler,
   initSockeIo,
   newHandler,
+  initSocketEvents,
 };
 export default toolkit;
