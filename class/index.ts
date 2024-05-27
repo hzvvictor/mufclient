effdfb824716bb95e115535334337a57c21bb473
hzvvictor <hzvvictor@gmail.com>
2024-05-26 18:37:28 -0600

    feat: Add APIClient class and export it in index.ts

--- class/index.ts
+++ class/index.ts
@@ -2,11 +2,14 @@ import SocketEvents, { newSocketEvents } from "./SocketEvents";
 export { SocketEvents, newSocketEvents };
 import StateNs from "./StateNs";
 export { StateNs }; 
+import APIClient from "./APIClient";
+export { APIClient };
 
 const Class = {
   SocketEvents,
   newSocketEvents,
-  StateNs
+  StateNs,
+  APIClient,
 };
 
 export default Class;
